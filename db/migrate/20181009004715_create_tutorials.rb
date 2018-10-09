a26c0bfffcdcd5d5c5898d9ba8314a6c63fe1b56
Trac D. Nguyen <nguyendtrac@gmail.com>
2018-10-08 20:44:40 -0500

    changed body to content in tutorials table

--- db/migrate/20181009004715_create_tutorials.rb
+++ db/migrate/20181009004715_create_tutorials.rb
@@ -2,7 +2,7 @@ class CreateTutorials < ActiveRecord::Migration[5.2]
   def change
     create_table :tutorials do |t|
       t.string :title
-      t.string :body
+      t.string :content
 
       t.timestamps
     end
