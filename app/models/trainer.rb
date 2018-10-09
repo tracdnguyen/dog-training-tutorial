579dafe3d5ea364f7c85525088e0c4856ee64709
Trac D. Nguyen <nguyendtrac@gmail.com>
2018-10-08 20:30:01 -0500

    add association data to trainer model

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -3,4 +3,7 @@ class Trainer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :comments
+  has_many :tutorials, through: :comments
 end
