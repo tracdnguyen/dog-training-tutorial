98012b82a10969c92fefb66fd182f84b53e83351
Trac D. Nguyen <nguyendtrac@gmail.com>
2018-10-11 17:05:08 -0500

    create action can now create a comment associated with a user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,20 @@
 class CommentsController < ApplicationController
   before_action :load_tutorial
   before_action :authenticate_trainer!
-  
+
   def new
 
   end
 
   def create
-
+    @comment = @tutorial.comments.build(comment_params)
+    @comment.trainer_id = current_trainer.id
+    if @comment.save
+      flash[:notice] = "Successfully Created Comment"
+      redirect_to tutorial_path(@tutorial)
+    else
+      render 'new'
+    end
   end
 
   private
