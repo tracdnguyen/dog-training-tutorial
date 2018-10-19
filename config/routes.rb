d1f14be6958803a50e8383cdecc9a1a0f73bdc20
Trac D. Nguyen <nguyendtrac@gmail.com>
2018-10-19 18:46:19 -0500

    added route and link in navbar to view trainer tutorials

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :trainers, controllers: { omniauth_callbacks: 'trainers/omniauth_callbacks' }
   root 'tutorials#index'
-  get 'trainer/tutorials', to: 'tutorials#trainer_tutorials'
+  get 'tutorials/trainer_tutorials' => "tutorials#trainer_tutorials"
   get 'tutorials/with_comments', to: 'tutorials#with_comments'
 
-  resources :tutorials
-
-  resources :tutorials do
-    resources :comments
+  resources :tutorials, only: [:show] do
+    resources :comments, only: [:create, :update, :destroy, :show, :index, :edit, :new]
   end
 
+
+  resources :tutorials, only: [:index, :create, :show, :edit, :update, :destroy, :new]
 end
