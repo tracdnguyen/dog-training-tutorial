39dd9d8da741ab7dd8ec942e4ed8d367c5609848
Trac D. Nguyen <nguyendtrac@gmail.com>
2018-10-23 16:07:38 -0500

    fixed routing syntax and appended a / in front of view your tutorial link in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   devise_for :trainers, controllers: { omniauth_callbacks: 'trainers/omniauth_callbacks' }
   root 'tutorials#index'
-  get 'tutorials/trainer_tutorials' => "tutorials#trainer_tutorials"
+  get 'tutorials/trainer_tutorials', to: 'tutorials#trainer_tutorials'
   get 'tutorials/with_comments', to: 'tutorials#with_comments'
 
   resources :tutorials do
     resources :comments
   end
-
 
   resources :tutorials
 end
