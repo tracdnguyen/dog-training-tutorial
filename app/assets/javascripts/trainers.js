ad851309c9cbdf52b1bca2de55328ab31bb46f26
Trac D. Nguyen <nguyendtrac@gmail.com>
2018-12-20 14:52:47 -0600

    created ajax request to log data

--- app/assets/javascripts/trainers.js
+++ app/assets/javascripts/trainers.js
@@ -1,6 +1,15 @@
-$( document ).on('turbolinks:load', function () {
-  $("a.load_tutorials").on('click', function (e) {
-    e.preventDefault();
-    alert("You clicked!");
-  })
+$(document).on('turbolinks:load', function () {
+  LoadTutorialsOnTrainersShow();
 })
+
+function LoadTutorialsOnTrainersShow () {
+  $("a.load_tutorials").on('click', function (e) {
+    $.ajax({
+      method: "GET",
+      url: this.href
+    }).done(function(data){
+      console.log(data)
+    });
+    e.preventDefault();
+  })
+}
