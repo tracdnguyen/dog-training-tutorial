5820f16b1a37aaa3b6eb0b97fb4193def8252955
Trac D. Nguyen <nguyendtrac@gmail.com>
2019-01-06 17:41:39 -0600

    changed tutorials.js to comments.js and created contructor syntax

---
+++ app/assets/javascripts/comments.js
@@ -0,0 +1,7 @@
+class Comment {
+  constructor(obj) {
+    this.tutorial_id = obj.tutorial_id;
+    this.trainer_id = obj.trainer_id;
+    this.content = obj.content;
+  }
+}
--- app/assets/javascripts/tutorials.js
+++
@@ -1,7 +0,0 @@
-class Tutorial {
-  constructor(obj) {
-    this.title = obj.title
-    this.content = obj.content
-    this.video_link = obj.video_link
-  }
-}
