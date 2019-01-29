88d2e326c0ee82b6764966d9aebb31e2f3f1ae98
Trac D. Nguyen <nguyendtrac@gmail.com>
2019-01-28 20:03:47 -0600

    added tutorials.js and created tutorial class

---
+++ app/assets/javascripts/tutorials.js
@@ -0,0 +1,9 @@
+class Tutorial {
+  constructor(obj) {
+    this.id = obj.id
+    this.content = obj.content
+    this.title = obj.title
+    this.video_link = obj.video_link
+    this.comments = obj.comments
+  }
+}
