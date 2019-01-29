05945bcf900cf27a7a4319066c3ea620a119cf0f
Trac D. Nguyen <nguyendtrac@gmail.com>
2019-01-28 20:07:24 -0600

    created method n tutorial object prototype to format comments

--- app/assets/javascripts/tutorials.js
+++ app/assets/javascripts/tutorials.js
@@ -13,3 +13,20 @@ Tutorial.prototype.formatVideoLink = () => {
     Link To Video: <a href="${this.video_link}">${this.video_link}</a>
   `)
 }
+
+Tutorial.prototype.showNextComments = (page) => {
+  return (`
+    <li>
+      <div class="container">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <strong>${this.comments[page]["trainer_email"]}</strong> <span class="text-muted"> said: </span>
+          </div>
+          <div class="panel-body">
+            ${this.comments[page]["content"]}</br>
+          </div>
+        </div>
+      </div>
+    </li>
+  `)
+}
