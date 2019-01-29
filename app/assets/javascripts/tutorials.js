103459ad0f58b37cf3b31f8c8c622e824870e2f7
Trac D. Nguyen <nguyendtrac@gmail.com>
2019-01-28 20:44:36 -0600

    next button on tutorial show page loads next pages data and comments, updated method on object prototype to work

--- app/assets/javascripts/tutorials.js
+++ app/assets/javascripts/tutorials.js
@@ -1,29 +1,29 @@
 class Tutorial {
   constructor(obj) {
-    this.id = obj.id
-    this.content = obj.content
-    this.title = obj.title
-    this.video_link = obj.video_link
-    this.comments = obj.comments
+    this.id = obj.id;
+    this.content = obj.content;
+    this.title = obj.title;
+    this.video_link = obj.video_link;
+    this.comments = obj.comments;
   }
 }
 
-Tutorial.prototype.formatVideoLink = () => {
+Tutorial.prototype.formatVideoLink = function () {
   return (`
     Link To Video: <a href="${this.video_link}">${this.video_link}</a>
   `)
 }
 
-Tutorial.prototype.showNextComments = (page) => {
+Tutorial.prototype.showNextComments = (comment) => {
   return (`
     <li>
       <div class="container">
         <div class="panel panel-default">
           <div class="panel-heading">
-            <strong>${this.comments[page]["trainer_email"]}</strong> <span class="text-muted"> said: </span>
+            <strong>${comment.trainer_email}</strong> <span class="text-muted"> said: </span>
           </div>
           <div class="panel-body">
-            ${this.comments[page]["content"]}</br>
+            ${comment.content}</br>
           </div>
         </div>
       </div>
