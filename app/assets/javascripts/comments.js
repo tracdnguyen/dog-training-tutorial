8479009645f6b1aeb479e4d471559f722793eb5f
Trac D. Nguyen <nguyendtrac@gmail.com>
2019-01-28 19:19:56 -0600

    added trainer_email to comment constructor and created prototype method to format comment

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -3,11 +3,29 @@ class Comment {
     this.tutorial_id = obj.tutorial_id;
     this.trainer_id = obj.trainer_id;
     this.content = obj.content;
+    this.trainer_email = obj.trainer_email
   }
 }
 
 Comment.prototype.commentHTML = function () {
   return (`
     <li>${this.content}</li>
+  `)
+}
+
+Comment.prototype.createCommentAjax = function() {
+  return (`
+    <li>
+      <div class="container">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <strong>${this.trainer_email}</strong> <span class="text-muted"> said: </span>
+          </div>
+          <div class="panel-body">
+            ${this.content}</br>
+          </div>
+        </div>
+      </div>
+    </li>
   `)
 }
